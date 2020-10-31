c2b1024f398cdb884632f195e413ec14667ef87e
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2020-10-31 17:52:14 +0300

    cow

---
+++ index.js
@@ -0,0 +1,10 @@
+const myHello = require('./information');
+myHello();
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "Hello, I'm " + data.name + " from campus " + data.campus + ".",
+	e : "oO",
+	T : "U "
+}));
